cff0625a57e38753518c4f247d2cb60d76c93d6c
AR-SB <aliridasiblani@outlook.com>
2023-08-13 17:15:46 +0300

    procedure added

--- Task1/Controllers/ApplicationController.cs
+++ Task1/Controllers/ApplicationController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Http;
 using Task1.Data;
 using Task1.Models;
 using System.IO;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Task1.Controllers
 {
     public class ApplicationController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IConfiguration _configuration;
 
-        public ApplicationController(ApplicationDbContext db)
+        public ApplicationController(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            _configuration = configuration;
         }
 
         // GET
@@ -46,17 +51,44 @@ namespace Task1.Controllers
                 }
 
                 obj.FileName = uniqueFileName;
-                _db.Application.Add(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ModelState.AddModelError("PdfFile", "Please choose a PDF file.");
-                return View(obj);
-            }
-        }
 
+                // Check the email using the stored procedure
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("CheckEmailForApplication", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@EmailAddress", obj.Email);
+                        SqlParameter canApplyParameter = command.Parameters.Add("@CanApply", SqlDbType.Bit);
+                        canApplyParameter.Direction = ParameterDirection.Output;
+
+                        command.ExecuteNonQuery();
+
+                        bool canApply = (bool)canApplyParameter.Value;
+                        if (!canApply)
+                        {
+                            ModelState.AddModelError("", "You can't apply within the same address in less than 24 hours.");
+                            Console.WriteLine("Email validation: Can't apply within 24 hours");
+                            return View(obj);
+                        }
+                        else
+                        {
+                            _db.Application.Add(obj);
+                            _db.SaveChanges();
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                }
+            }
+
+            // Return a default view if the file condition is not met
+            ModelState.AddModelError("", "Please choose a PDF file.");
+            return View(obj);
+        }
 
     }
 }
+
